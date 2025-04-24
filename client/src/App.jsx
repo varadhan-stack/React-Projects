c3530227461730b8345b93ea74622896bfdda76f
Nayagan <varadhandpi@gmail.com>
2025-04-24 19:51:26 +0530

    UI order page update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,11 +39,14 @@ const App = () => {
           <Route path='/cart' element={<Cart />} />
           <Route path='/add-address' element={<AddAddress />} />
           <Route path='/my-orders' element={<MyOrders />} />
+          
+          {/* Admin Account Routers */}
           <Route path='/seller' element={isSeller ? <SellerLayout /> : <SellerLogin />} >
             <Route index element={isSeller ? <AddProduct /> : null} />
             <Route path='product-list' element={<ProductList />} />
             <Route path='orders' element={ <Orders />} />
           </Route>
+
         </Routes>
       </div>
       
